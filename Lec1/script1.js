fa1c6f8d3cb51590ac3713024188172d10887af7
VISHAL <vishalstar895@gmail.com>
2023-11-06 23:25:35 +0530

    backend

---
+++ Lec1/script1.js
@@ -0,0 +1,9 @@
+var dd = require('./script2')
+
+var a =12;
+
+var b = 22;
+
+var c = 99 ;
+console.log(dd);
+
---
+++ Lec1/script2.js
@@ -0,0 +1,6 @@
+var d  = 33;
+
+var e = 54+ "https://twitter.com/i/status/1720515888430907706   https:/terabox.app/s/1zj2zlAUhTvumSLbIFJHxMw";
+
+module.exports = {first:d,second:e};
+
