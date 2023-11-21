8159385fd5e1b88fce723be45a86e7a88df26a39
Vishal <31177415+vishal-kt@users.noreply.github.com>
2023-11-21 14:54:38 +0000

    basic routing

--- Lec2/express1.js
+++ Lec2/express1.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 app.get("/",function (req,res) {
-    res.send("hey")
+    res.send("hello lodu")
+})
+
+app.get("/profile",function (req,res) {
+    res.send("hello vishal")
 })
 
 app.listen(3000);
