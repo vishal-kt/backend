125695fe617eea2413c56f7c838075ae16a42b54
Vishal <31177415+vishal-kt@users.noreply.github.com>
2023-11-24 12:10:39 +0000

    update

--- FirstProject/Project1.js
+++ FirstProject/Project1.js
@@ -4,7 +4,7 @@ const app = express()
 app.set("view engine","ejs");
 
 app.get("/",function (req,res) {
-    res.render("index");
+    res.render("index", {Home:"🏠",Contact:"📞", Info :"ℹ"});
 });
 
 app.listen(3000, function () {
